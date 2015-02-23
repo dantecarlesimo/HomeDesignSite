b19c9f5556c9198992b5263f5259f485708a3f3f
unknown <dcarlesimo@dcarlesimo-w7.luthresearch.net>
2015-02-23 14:50:34 -0800

    cleaned script code

--- script.js
+++ script.js
@@ -30,23 +30,3 @@ $(document).ready(function() {
     });
 
 });
-
-
-//$('p').html("changing the p text");
-// $('p').append("testing more");
-// $('div.homeBox').append("<li>this is a LI</li>");
-//  $('#mouseStuff').append("<p> YAY! </p>");
-
-//$('div.collapse').accordion({collapsible: true, active: true});
-
-
-
-//  $('.container').mouseover(function() {
-//      alert("hover");
-//     $('#imgContainer').append("<p> YAY! </p>");
-//
-//
-//
-//});
-
-//  $('.text').hide().removeClass('text').addclass('text-js');
